c0c003d985612498545d755553f1b8b941baf851
Altan-Ochir <altanochir0130@yahoo.com>
2021-07-26 10:31:08 -0500

    work 3 is done

--- src/day1/Second.java
+++ src/day1/Second.java
@@ -4,7 +4,8 @@ public class Second {
     public static void main(String[] args) {
 
         // work 1 is done here
-        // wotk 2 is done here
+        // work 2 is done here
+        // work 3 is done here
 
     }
 }
