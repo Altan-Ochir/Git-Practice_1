27d80d0a6a6e3cbe5314923167cab09d616a44ea
Altan-Ochir <altanochir0130@yahoo.com>
2021-07-29 13:34:26 -0500

    added new thing

--- src/day03/ListPractice.java
+++ src/day03/ListPractice.java
@@ -1,5 +1,12 @@
 package day03;
 
-public class ListPractice {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class ListPractice {
+    public static void main(String[] args) {
+        ArrayList<String> lst = new ArrayList<>();
+        // added new thing
+
+    }
 }
