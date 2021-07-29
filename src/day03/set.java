ab47401991b21fe144a26efb20550ef4ce7c6474
Altan-Ochir <altanochir0130@yahoo.com>
2021-07-29 13:46:00 -0500

    feature is added

--- src/day03/set.java
+++ src/day03/set.java
@@ -4,6 +4,6 @@ public class set {
     public static void main(String[] args) {
 
     //set is a collection type that only store unique item
-
+    // if you add duplicate it will just remove it
     }
 }
